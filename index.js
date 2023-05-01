79b3009244b127cf53b103d769bfa3f4b0b1a043
Alex Iats <chava160389@gmail.com>
2023-05-01 16:56:36 +0100

    feat: :sparkles: login and check status via token done

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ import mongoose from 'mongoose';
 import bcrypt from 'bcrypt';
 import { validationResult } from 'express-validator';
 import { registerValidator } from './validations/auth.js';
-import UserModel from './models/User.js'
+import UserModel from './models/User.js';
+import checkAuth from './utils/checkAuth.js'
 
 mongoose
     .connect('mongodb+srv://admin:wwwwww@cluster0.8ny2mnh.mongodb.net/?retryWrites=true&w=majority')
@@ -22,6 +23,47 @@ app.listen(4444, (err) => {
     console.log('Server OK')
 })
 
+app.post('/auth/login', async (req, res) => {
+    try {
+        const user = await UserModel.findOne({email: req.body.email});
+
+        if (!user) {
+            res.status(404).json({
+                message: 'Wrong email or password'
+            })
+        }
+
+        const isValidPass = await bcrypt.compare(req.body.password, user.passwordHash)
+
+        if (!isValidPass) {
+            res.status(400).json({
+                message: 'Wrong email or password'
+            })
+        }
+
+        const token = jwt.sign({
+            _id: user._id,
+        }, 
+        'secret123',
+        {
+            expiresIn: '30d'
+        });
+
+        const {passwordHash, ...userData} = user._doc;
+
+        res.json({
+            ...userData,
+            token
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            message: 'Cant login',
+            error
+        })
+    }
+})
+
 app.post('/auth/register', registerValidator, async (req, res) => {
     try {
         const errors = validationResult(req)
@@ -30,16 +72,52 @@ app.post('/auth/register', registerValidator, async (req, res) => {
         }
         const { password } = req.body;
         const salt = await bcrypt.genSalt(10);
-        const passwordHash = await bcrypt.hash(password, salt);
+        const hash = await bcrypt.hash(password, salt);
 
         const doc = new UserModel({
             email: req.body.email,
             fullName: req.body.fullName,
             avatarUrl: req.body.avatarUrl,
-            passwordHash
+            passwordHash: hash,
         })
+        
         const user = await doc.save();
-        res.json(user)
+        const token = jwt.sign({
+            _id: user._id,
+        }, 
+        'secret123',
+        {
+            expiresIn: '30d'
+        });
+
+        const {passwordHash, ...userData} = user._doc;
+
+        res.json({
+            ...userData,
+            token
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            message: 'Cant register',
+            error
+        })
+    }
+})
+
+app.get('/auth/me', checkAuth, async (req, res) => {
+    try {
+        const user = await UserModel.findById(req.userId);
+
+        if (!user) {
+            req.status(404).json({
+                message: "No user founded"
+            })
+        }
+        
+        const {passwordHash, ...userData} = user._doc;
+
+        res.json(userData);
     } catch (error) {
         res.status(500).json({
             message: 'Cant register',
